96b74e0eef38b8075c3c79c86f723141fd0da120
alseddiq <abdallahalseddek@gmail.com>
2024-01-03 23:30:28 +0200

    Menu-Restaurant Api Design

--- src/main/java/com/javaeat/controller/MenuController.java
+++ src/main/java/com/javaeat/controller/MenuController.java
@@ -1,30 +1,73 @@
 package com.javaeat.controller;
 
 
+import com.javaeat.request.MenuItemRequest;
+import com.javaeat.request.MenuRequest;
+import com.javaeat.services.MenuService;
+import com.javaeat.util.ApiResponse1;
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Arrays;
 
 @RestController
 @RequestMapping("api/menu")
 @Slf4j
 @Tag(name = "Menu Endpoints")
+@RequiredArgsConstructor
 public class MenuController {
+    private final MenuService menuService;
 
-    @GetMapping("/{restaurantId}")
-    @Operation(summary = "Check Restaurant acceptance.",
-            description = "Endpoint that checks whether it accepts orders or not.")
-    @ApiResponse(responseCode = "200", description = "Successful Operation")
-    @ApiResponse(responseCode = "404", description = "Order Not Found Exception")
-    public ResponseEntity<Boolean> checkRestaurantAcceptance(@PathVariable Integer restaurantId) {
+    @PostMapping("/addMenu")
+    @Operation(summary = "Add a new Menu", description = "Add a new Menu")
+    public ResponseEntity<ApiResponse1> addMenu(@RequestBody MenuRequest menuRequest) {
+        return ApiResponse1.createUnifiedResponse(
+                menuService.addMenu(menuRequest),
+                HttpStatus.CREATED, "Successful operation",
+                Arrays.asList("Bad request. Invalid input data",
+                        "Menu not found or Item not Found",
+                        "Internal server error. Something went wrong"));
+    }
 
-        //return a fake status
-        return ResponseEntity.ok(Boolean.TRUE);
+    @PatchMapping("/addMenuItem")
+    @Operation(summary = "Add a new Menu Item", description = "Add a new Menu Item")
+    public ResponseEntity<ApiResponse1> addMenuItem(@RequestBody MenuItemRequest menuItemRequest) {
+        return ApiResponse1.createUnifiedResponse(
+                menuService.addMenuItem(menuItemRequest),
+                HttpStatus.CREATED, "Successful operation",
+                Arrays.asList("Bad request. Invalid input data",
+                        "Menu Item Info not found or Item not Found",
+                        "Internal server error. Something went wrong"));
+    }
+
+    @PatchMapping("/updateMenuItem")
+    @Operation(summary = "update Menu Item info", description = "update Menu Item info")
+    public ResponseEntity<ApiResponse1> updateMenuItem(@RequestBody MenuItemRequest menuItemRequest) {
+        return ApiResponse1.createUnifiedResponse(
+                menuService.updateMenuItem(menuItemRequest),
+                HttpStatus.CREATED, "Successful operation", Arrays.asList("Bad request. Invalid input data",
+                        "Menu Item Info not found or Item not Found",
+                        "Internal server error. Something went wrong"));
+    }
+
+    @DeleteMapping("/deleteMenu/{menuId}")
+    @Operation(summary = "delete Restaurant menu", description = "delete Restaurant menu")
+    public void deleteMenu(@PathVariable Integer menuId) {
+        menuService.deleteMenu(menuId);
+    }
+    @DeleteMapping("/clearMenu/{menuId}")
+    @Operation(summary = "Make menu Empty", description = "Make menu Empty")
+    public void clearMenu(@PathVariable Integer menuId) {
+        menuService.clearMenu(menuId);
+    }
+    @DeleteMapping("/deleteMenuItem/{menuItemId}")
+    @Operation(summary = "delete Menu Item", description = "delete Menu Item")
+    public void deleteMenuItem(@PathVariable Integer menuItemId) {
+        menuService.deleteMenuItem(menuItemId);
     }
 }
--- src/main/java/com/javaeat/controller/RestaurantController.java
+++ src/main/java/com/javaeat/controller/RestaurantController.java
@@ -1,31 +1,67 @@
 package com.javaeat.controller;
 
 
-import com.javaeat.request.OrderResponse;
+import com.javaeat.request.RestaurantRequest;
+import com.javaeat.services.RestaurantService;
+import com.javaeat.util.ApiResponse1;
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Arrays;
 
 @RestController
 @RequestMapping("api/restaurant")
 @Slf4j
 @Tag(name = "Restaurant Endpoints")
+@RequiredArgsConstructor
 public class RestaurantController {
+    private final RestaurantService restaurantService;
 
-    @GetMapping("/{restaurantId}")
-    @Operation(summary = "Check Restaurant acceptance.",
-            description = "Endpoint that checks whether it accepts orders or not.")
-    @ApiResponse(responseCode = "200", description = "Successful Operation")
-    @ApiResponse(responseCode = "404", description = "Order Not Found Exception")
-    public ResponseEntity<Boolean> checkRestaurantAcceptance(@PathVariable Integer restaurantId) {
-
-        //return a fake status
-        return ResponseEntity.ok(Boolean.TRUE);
+    @PostMapping("/addRestaurant")
+    @Operation(summary = "Add a new Restaurant", description = "Add a new Restaurant")
+    public ResponseEntity<ApiResponse1> addNewRestaurant(@RequestBody RestaurantRequest restaurantRequest) {
+        return ApiResponse1.createUnifiedResponse(
+                        restaurantService.addNewRestaurant(restaurantRequest),
+                        HttpStatus.CREATED, "Successful operation", Arrays.asList("Bad request. Invalid input data",
+                                "Restaurant not found or Item not Found",
+                                "Internal server error. Something went wrong"));
     }
+    @PatchMapping("/updateRestaurant")
+    @Operation(summary = "update Restaurant info", description = "update Restaurant info")
+    public ResponseEntity<ApiResponse1> updateRestaurant(@RequestBody RestaurantRequest restaurantRequest) {
+        return ApiResponse1.createUnifiedResponse(
+                restaurantService.updateRestaurant(restaurantRequest),
+                HttpStatus.CREATED, "Successful operation", Arrays.asList("Bad request. Invalid input data",
+                        "Restaurant not found or Item not Found",
+                        "Internal server error. Something went wrong"));
+    }
+    @GetMapping("/listRestaurantMenus/{restaurantId}")
+    @Operation(summary = "Add a new Restaurant", description = "Add a new Restaurant")
+    public ResponseEntity<ApiResponse1> listAllRestaurantMenus(@PathVariable Integer restaurantId) {
+        return ApiResponse1.createUnifiedResponse(
+                restaurantService.listAllRestaurantMenus(restaurantId),
+                HttpStatus.CREATED, "Successful operation", Arrays.asList("Bad request. Invalid input data",
+                        "Restaurant not found or Item not Found",
+                        "Internal server error. Something went wrong"));
+    }
+    @GetMapping("/getRating/{restaurantId}")
+    @Operation(summary = "get the rating of Restaurant", description = "get the rating of Restaurant")
+    public ResponseEntity<ApiResponse1> getRestaurantRating(@PathVariable Integer restaurantId) {
+        return ApiResponse1.createUnifiedResponse(
+                restaurantService.getRestaurantRating(restaurantId),
+                HttpStatus.CREATED, "Successful operation", Arrays.asList("Bad request. Invalid input data",
+                        "Restaurant not found or Item not Found",
+                        "Internal server error. Something went wrong"));
+    }
+    @DeleteMapping("/deleteRestaurant/{restaurantId}")
+    @Operation(summary = "Add a new Restaurant", description = "Add a new Restaurant")
+    public void deleteRestaurant(@PathVariable Integer restaurantId) {
+        restaurantService.deleteRestaurant(restaurantId);
+    }
+
 }
