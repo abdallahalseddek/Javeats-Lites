c65f8f57299b29284220922badcb327988d6a368
alseddiq <abdallahalseddek@gmail.com>
2024-01-09 10:53:12 +0200

    menu controller unit test

--- src/main/java/com/javaeat/request/MenuItemRequest.java
+++ src/main/java/com/javaeat/request/MenuItemRequest.java
@@ -1,14 +1,12 @@
 package com.javaeat.request;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 @Setter
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class MenuItemRequest{
     private Integer id;
     private String title;
--- src/main/java/com/javaeat/response/MenuItemResponse.java
+++ src/main/java/com/javaeat/response/MenuItemResponse.java
@@ -1,14 +1,12 @@
 package com.javaeat.response;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 @Setter
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class MenuItemResponse {
     private Integer id;
     private String title;
